e0a75d83c5d367b71564cabfe50683107e0da81e
beatrizmcfonseca <74422646+beatrizmcfonseca@users.noreply.github.com>
2020-12-04 08:55:09 +0000

    Update regression.py
    Regressão linear de vários parâmetros

--- regression.py
+++ regression.py
@@ -1,4 +1,3 @@
-import numpy as np
 class Rlinear():
     def __init__(self,n_iteracoes=1000,min_error=0.001,min_delta=0.001,learning_rate=0.1):
         self.n_iteracoes=n_iteracoes
@@ -8,51 +7,13 @@ class Rlinear():
         
     def treino(self, X, Y):
         n_individuos, n_variaveis=X.shape
-        self.pesos=np.random.rand()
-
-#desenhar variáveis
-var_x = []
-nr_var_x = len(var_x)
-var_y = []
-
-#Calculate means
-#mean of x
-def mean(var_x):
-    for x in var_x:
-        mean_x = sum(var_x) / len (var_x)
-print ("The mean for variable x is {mean_x}")
-
-#mean of y
-def mean_y(var_y):
-    for y in var_y:
-        mean_y = sum(var_y) / len (var_y)
-print ("The mean for variable y is {mean_y}")
-
-#calculate distance to the mean
-#distance of variable x
-def distance_x(var_x):
-    for x in var_x:
-        distance_x = sum(var_x - mean_x)
-print ("The sum of distances to the mean for variable x is {distance_x}")
-
-#distance of squares of variable x:
-def square_distance(var_x):
-    for x in var_x:
-        square_distance = sum (var_x - mean_x)**2
-
-#distance of variable y
-def distance_x(var_y):
-    for y in var_y:
-        distance_y = sum(var_y - mean_y)
-print ("The sum of distances to the mean for variable y is {distance_y}")
-
-#calculate multiplication of distance to the mean
-def multiplication(var_x,var_y):
-    for distance in distance_x:
-        multiplication = distance_x * distance_y
-
-#calculate slope of the regression (b1)
-def slope(multiplication):
-    for x,y in multiplication:
-        slope = multiplication / square_distance
-print ("The slope of the linear regression is {slope}")
+        self.pesos=np.random.rand(n_variaveis,1)
+        for e in range(self.n_iteracoes):
+            for i in range(n_individuos):
+                y_prev=X[i,:].dot(self.pesos)
+                error=y_prev-Y[i]
+                self.pesos=self.pesos-self.learning_rate*(2*error*X[i,:].reshape(n_variaveis,1))
+                
+    def predict(self,X):
+        previsao=X.dot(self.pesos)
+        return previsao
