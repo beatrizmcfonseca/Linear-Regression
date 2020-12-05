f60212abaa605c40906d9de24e8808193eab68d2
beatrizmcfonseca <74422646+beatrizmcfonseca@users.noreply.github.com>
2020-12-05 00:12:10 +0000

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,46 +1,15 @@
-#desenhar variáveis
-var_x = []
-nr_var_x = len(var_x)
-var_y = []
-
-#Calculate means
-#mean of x
-def mean(var_x):
-    for x in var_x:
-        mean_x = sum(var_x) / len (var_x)
-print ("The mean for variable x is {mean_x}")
-
-#mean of y
-def mean_y(var_y):
-    for y in var_y:
-        mean_y = sum(var_y) / len (var_y)
-print ("The mean for variable y is {mean_y}")
-
-#calculate distance to the mean
-#distance of variable x
-def distance_x(var_x):
-    for x in var_x:
-        distance_x = sum(var_x - mean_x)
-print ("The sum of distances to the mean for variable x is {distance_x}")
-
-#distance of squares of variable x:
-def square_distance(var_x):
-    for x in var_x:
-        square_distance = sum (var_x - mean_x)**2
-
-#distance of variable y
-def distance_x(var_y):
-    for y in var_y:
-        distance_y = sum(var_y - mean_y)
-print ("The sum of distances to the mean for variable y is {distance_y}")
-
-#calculate multiplication of distance to the mean
-def multiplication(var_x,var_y):
-    for distance in distance_x:
-        multiplication = distance_x * distance_y
-
-#calculate slope of the regression (b1)
-def slope(multiplication):
-    for x,y in multiplication:
-        slope = multiplication / square_distance
-print ("The slope of the linear regression is {slope}")
+import RL
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv(r"C:\Users\beatr\OneDrive\Documentos\data science\programming\trabalho\insurance.csv")
+data.head()
+X = np.array(pd.DataFrame (data.iloc [:,:-1]))
+Y = np.array(pd.DataFrame (data.iloc [:,:-1]))
+Model = RL.Rlinear()
+Model.min_error
+plt.scatter(X,Y,color="red")
+plt.title('bmi vs age')
+plt.xlabel('bmi')
+plt.ylabel('age')
+plt.show()
